6757e6a0b61ba71e4be4567fae18e2f121f074c7
bighoggy <yoshi.yoshi0405@gmail.com>
2023-02-05 13:05:06 +0900

    Calendar program v3

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -4,25 +4,22 @@ require 'optparse'
 params = OptionParser.new
 params = ARGV.getopts("", "y:#{Date.today.year}", "m:#{Date.today.month}")
 
-year = params.values[0].to_i
 month = params.values[1].to_i
+year = params.values[0].to_i
 
-first_date = Date.new(year, month, 1).wday
+first_wday = Date.new(year, month, 1).wday
 end_date = Date.new(year, month, -1).day
 
-puts (month.to_s + "月" + " " + year.to_s + "年").center(21) 
-puts "\n"
+puts " #{month} 月 #{year} 年".center(21) 
 puts '日 月 火 水 木 金 土'
-space = "   " * first_date
+space = "   " * first_wday
 print space
 
 (1..end_date).each do |x|
-    if (first_date + x) % 7 == 0
-        print x.to_s.rjust(2) + "\n"
-    else
-        print x.to_s.rjust(2) + " "
-    end
+  if (first_wday + x) % 7 == 0
+    print x.to_s.rjust(2) + "\n"
+  else
+    print x.to_s.rjust(2) + " "
+  end
 end
 print "\n"
-
-
