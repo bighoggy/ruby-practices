203b0400b058c8cbcafe7f98749a17f767bf9054
bighoggy <yoshi.yoshi0405@gmail.com>
2023-04-23 11:21:32 +0900

    Modified bowling program v2

--- 03.bowling/score.rb
+++ 03.bowling/score.rb
@@ -13,47 +13,19 @@ scores.each do |s|
   end
 end
 
-frames = []
-shots.each_slice(2) do |s|
-  frames << s
-end
+frames = shots.each_slice(2).to_a
 
 total_point = 0
-index = 0
 
 frames.each_with_index do |frame, i|
-  if [10, 11].include?(i)
-    index += 2
-  elsif i == 9
-    total_point += if shots[index] == 10
-                     if shots[index + 2] == 10
-                       20 + shots[index + 4]
-                     else
-                       10 + shots[index + 2] + shots[index + 3]
-                     end
-                   elsif shots[index] + shots[index + 1] == 10
-                     10 + shots[index + 2]
-                   else
-                     shots[index] + shots[index + 1]
-                   end
-  elsif frame[0] == 10
-    total_point += if shots[index + 2] == 10
-                     10 + shots[index + 2] + shots[index + 4]
-                   else
-                     10 + shots[index + 2] + shots[index + 3]
-                   end
-    index += 2
-  elsif shots[index] + shots[index + 1] == 10
-    total_point += if shots[index + 2].nil?
-                     10
-                   else
-                     10 + shots[index + 2]
-                   end
-    index += 2
-  else
-    total_point += shots[index] + shots[index + 1]
-    index += 2
-  end
-end
+  break if i == 10
 
+  total_point += if frame[0] == 10
+                   10 + frames[i + 1][0] + (frames[i + 1][1] != 0 ? frames[i + 1][1] : frames[i + 2]&.[](0) || 0)
+                 elsif frame.sum == 10
+                   10 + frames[i + 1][0]
+                 else
+                   frame.sum
+                 end
+end
 puts total_point
