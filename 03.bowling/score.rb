32a2b231029ccddcab4388b70315010146cf52b8
bighoggy <yoshi.yoshi0405@gmail.com>
2023-05-13 22:48:00 +0900

    i hope this is going to be the final.

--- 03.bowling/score.rb
+++ 03.bowling/score.rb
@@ -20,23 +20,21 @@ total_point = 0
 frames.each_with_index do |frame, i|
   break if i == 10
 
-  strike = frame[0] == 10
-  spare = frame.sum == 10
-
   next_frame = frames[i + 1]
   next_next_frame = frames[i + 2]
 
-  if strike
-    bonus_point = next_frame[0] + (if next_frame[1] != 0
-                                     next_frame[1]
-                                   else
-                                     (next_next_frame ? next_next_frame[0] : 0)
-                                   end)
-    total_point += 10 + bonus_point
+  strike = frame[0] == 10
+  spare = frame.sum == 10
+  total_point += frame.sum
+
+  if strike && next_frame[0] == 10
+    bonus_point = next_frame[0] + next_frame[1] + next_next_frame[0]
+    total_point += bonus_point
+  elsif strike
+    bonus_point = next_frame[0] + next_frame[1]
+    total_point += bonus_point
   elsif spare
-    total_point += 10 + next_frame[0]
-  else
-    total_point += frame.sum
+    total_point += next_frame[0]
   end
 end
 puts total_point
