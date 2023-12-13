7486cab6f8bf1a42541e0fe6b6c070a1ea6375da
bighoggy <yoshi.yoshi0405@gmail.com>
2023-12-13 15:14:46 +0900

    fixing existing issue from previous practices

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,25 +1,27 @@
+# frozen_string_literal: true
+
 require 'date'
 require 'optparse'
 
-params = OptionParser.new
-params = ARGV.getopts("", "y:#{Date.today.year}", "m:#{Date.today.month}")
+OptionParser.new
+params = ARGV.getopts('', "y:#{Time.zone.today.year}", "m:#{Time.zone.today.month}")
 
-month = params["m"].to_i
-year = params["y"].to_i
+month = params['m'].to_i
+year = params['y'].to_i
 
 first_wday = Date.new(year, month, 1).wday
 end_date = Date.new(year, month, -1).day
 
-puts " #{month} 月 #{year} 年".center(21) 
+puts " #{month} 月 #{year} 年".center(21)
 puts '日 月 火 水 木 金 土'
-space = "   " * first_wday
+space = '   ' * first_wday
 print space
 
 (1..end_date).each do |x|
-  if (first_wday + x) % 7 == 0
-    print x.to_s.rjust(2) + "\n"
+  if ((first_wday + x) % 7).zero?
+    print "#{x.to_s.rjust(2)}\n"
   else
-    print x.to_s.rjust(2) + " "
+    print "#{x.to_s.rjust(2)} "
   end
 end
 print "\n"
